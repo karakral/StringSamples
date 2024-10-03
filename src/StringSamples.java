f68ebd413188b257ebc34708de9b7629512b7b11
karakral <mahsun.fahmy@gmail.com>
2024-10-03 21:02:04 +0330

    add: main method for test methods: MethodsStringBuilder StringFamily StringSamples

---
+++ src/StringSamples.java
@@ -0,0 +1,9 @@
+public class StringSamples {
+    public static void main(String[] args) {
+    StringFamily stringFamily = new StringFamily();
+    stringFamily.StringFamily();
+    MethodsStringBuilder methodsStringBuilder = new MethodsStringBuilder();
+    methodsStringBuilder.methodsStringBuilder();
+    }
+}
+
