ae1af09c6a665579269db1ff1adf9cd74c139eae
karakral <mahsun.fahmy@gmail.com>
2024-10-20 12:56:49 +0330

    uncommitd

--- src/StringSamples.java
+++ src/StringSamples.java
@@ -19,21 +19,21 @@ public class StringSamples {
 
 //_______________________ test why we override hashcode and equal when useing hashcode Hashpam
 //
-//        HashMap<Person, String> map = new HashMap<Person, String>();
-//
-//        Person person1 = new Person("Alice", 30);
-//        Person person2 = new Person("Alice", 30);
-//
-//        map.put(person1, "Person 1");
-//        map.put(person2, "Person 2");
-//
-//        // Should print only one entry if hashCode() and equals() are overridden correctly
-//        System.out.println("Map size: " + map.size()); // Should be 1
-//
-//        // Checking value associated with key person1
-//        System.out.println("Value for person1: " + map.get(person1)); // Should re
-//        ArrayList<String> cars = new ArrayList<String>();
-//        cars.add("dg");
+        HashMap<Person, String> map = new HashMap<Person, String>();
+
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Alice", 30);
+
+        map.put(person1, "Person 1");
+        map.put(person2, "Person 2");
+
+        // Should print only one entry if hashCode() and equals() are overridden correctly
+        System.out.println("Map size: " + map.size()); // Should be 1
+
+        // Checking value associated with key person1
+        System.out.println("Value for person1: " + map.get(person1)); // Should re
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("dg");
 
     }
 }
